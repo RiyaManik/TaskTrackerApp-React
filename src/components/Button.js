2a920a3f982af0849091b8752a7378d8d3c5904f
RiyaManik <riyamanik8800@gmail.com>
2023-07-10 00:01:11 +0530

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+
+const onClick = (e) => {
+    
+}
+const Button = ({color, text, onClick}) => {
+  return <button onClick={onClick} style={{backgroundColor: color}} className="btn">{text}</button>
+}
+
+export default Button
